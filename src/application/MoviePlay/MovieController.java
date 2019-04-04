2ca1d38835ff9dc99ed4e9a82d3fec7dcd98ca1e
D070429 <christian.gunzelmann@sap.com>
2019-04-04 13:28:27 +0200

    Labelsetzung

--- src/application/MoviePlay/MovieController.java
+++ src/application/MoviePlay/MovieController.java
@@ -2,16 +2,20 @@ package application.MoviePlay;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
 
 import application.datamodel.Film;
+import application.datamodel.Genre;
+import application.datamodel.Plattform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -32,7 +36,24 @@ public class MovieController implements Initializable {
 	private JFXButton Playbutton;
 	
 	private MediaPlayer mediaPlayer;
+	
 	private Film film;
+	
+	@FXML
+	private Label la_title;
+	@FXML
+	private Label la_description;
+	@FXML
+	private Label la_duration;
+	@FXML
+	private Label la_fsk;
+	@FXML
+	private Label la_genre;
+	@FXML
+	private Label la_plattform;
+	
+	
+	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -42,6 +63,7 @@ public class MovieController implements Initializable {
 		mediaPlayer = new MediaPlayer(media);
 		
 		mediaView.setMediaPlayer(mediaPlayer);
+		
 		
 	}
 	
@@ -73,6 +95,44 @@ public class MovieController implements Initializable {
 	
 	public void setMovie(Film film) {
 		this.film= film;
+	}
+	
+	public void setLables() {
+		
+		la_title.setText(film.getTitle());
+		la_description.setText(film.getDescription());
+		la_duration.setText("Dauer: "+film.getDuration()+ "Minuten");
+		la_fsk.setText("Altersbeschränkung: " + film.getFsk());
+		
+		List<Genre> genres = film.getGenreList();
+		
+		String genreLabel = "Genres: ";
+		
+		for(int i = 0; i < genres.size()-1; i++) {
+			
+			genreLabel = genreLabel + genres.get(i).getName() +", ";
+			
+		}
+		
+		genreLabel = genreLabel + genres.get(genres.size()-1).getName();
+		
+		la_genre.setText(genreLabel);
+		
+		
+		List<Plattform> plattforms = film.getPlattformList();
+		
+		String plattformLabel = "Plattforms: ";
+		
+		for(int i = 0; i < plattforms.size()-1; i++) {
+			
+			plattformLabel = plattformLabel + plattforms.get(i).getName() +", ";
+			
+		}
+		
+		plattformLabel = plattformLabel + plattforms.get(plattforms.size()-1).getName();
+		
+		la_plattform.setText(plattformLabel);
+		
 	}
 
 }
